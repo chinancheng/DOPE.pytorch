ccb17d022aa49a0f52d683f72911b4201744153e
chinancheng <chinancheng0811@gmail.com>
2018-12-14 21:28:57 +0800

    fix bug

--- demo-pnp.py
+++ demo-pnp.py
@@ -20,17 +20,17 @@ def convert_rvec_to_quaternion(rvec):
     return Quaternion.from_axis_rotation(raxis, theta)
 
 
-def draw(img, vertexes, color=(0, 0, 255)):
+def draw(img, vertexes, color=(0, 0, 255), scale=1):
     for point in vertexes:
         if point:
-            point = tuple([int(p * Config.stride * ratio) for p in point])
+            point = tuple([int(p*scale) for p in point])
             img = cv2.circle(img, point, 5, color, -1)
     line_1 = [0, 1, 5, 4, 4, 0, 1, 5, 7, 3, 2, 6]
     line_2 = [1, 5, 4, 0, 7, 3, 2, 6, 3, 2, 6, 7]
     for p1, p2 in zip(line_1, line_2):
         if vertexes[p1] and vertexes[p2]:
-            p1 = tuple([int(p * Config.stride * ratio) for p in vertexes[p1]])
-            p2 = tuple([int(p * Config.stride * ratio) for p in vertexes[p2]])
+            p1 = tuple([int(p * scale) for p in vertexes[p1]])
+            p2 = tuple([int(p * scale) for p in vertexes[p2]])
             img = cv2.line(img, p1, p2, color=color, thickness=1)
 
     return img
@@ -38,12 +38,13 @@ def draw(img, vertexes, color=(0, 0, 255)):
 model = get_model(trunk='vgg19')
 model = model.cuda()
 use_vgg(model, './model', 'vgg19')
-model.load_state_dict(torch.load('./logs-drv3-2/checkpoint.pth'))
+model.load_state_dict(torch.load('./logs/logs-drv3-2/checkpoint.pth'))
 model.eval()
-path_to_data = '/media/ssd_external/DR-V3'
+# path_to_data = '/media/ssd_external/DR-V3'
 # path_to_data = './data/JockDM'
 # path_to_data = './data/real'
-f = open(os.path.join(path_to_data, 'train.txt'))
+path_to_data = './data/JPEGImages'
+# f = open(os.path.join(path_to_data, 'test.txt'))
 
 
 # parameter of object and camera for pnp solver
@@ -57,16 +58,24 @@ matrix_camera[1,1] = intrinsic_settings['fy']
 matrix_camera[0,2] = intrinsic_settings['cx']
 matrix_camera[1,2] = intrinsic_settings['cy']
 matrix_camera[2,2] = 1
+# matrix_camera[0,0] = 941.45
+# matrix_camera[1,1] = 971.15
+# matrix_camera[0,2] = 637.62
+# matrix_camera[1,2] = 348.42
+# matrix_camera[2,2] = 1
 dist_coeffs = np.zeros((4,1))
 
 # path_to_sequences = [os.path.join(path_to_data, '%d.jpg' %(int(line.split('\n')[0]))) for line in f.readlines()]
-path_to_sequences = [os.path.join(path_to_data, '%.6d.png' %(int(line.split('\n')[0]))) for line in f.readlines()]
+# path_to_sequences = [os.path.join(path_to_data, '%.6d.png' %(int(line.split('\n')[0]))) for line in f.readlines()]
+path_to_sequences = [os.path.join(path_to_data, 'Real_{}.jpg' .format(i)) for i in range(len(os.listdir(path_to_data)))]
+
 for path in path_to_sequences:
     original_img = cv2.imread(path)
     # original_img = cv2.imread('./data/BottleDataSet/JockMiddle/%.6d.png' %(id))
     # original_img = cv2.imread('./data/real/%d.jpg' % (id))
     original_img = crop(original_img)
     ratio = max(original_img.shape[:2]) / Config.crop_size
+    original_img = crop(original_img)
     img = cv2.resize(original_img, (Config.crop_size, Config.crop_size))
     img = preprocess(img).float()
     img = torch.unsqueeze(img, 0)
@@ -90,11 +99,13 @@ for path in path_to_sequences:
                 # Ignore invalid points
                 if (check_point_2d is None):
                     continue
+                else:
+                    check_point_2d = (check_point_2d[0] * Config.stride * ratio, check_point_2d[1] * Config.stride * ratio)
                 obj_2d_points.append(check_point_2d)
                 obj_3d_points.append(cuboid3d_points[i])
             projected_points = object[1]
             vertexes = projected_points
-            draw(original_img, vertexes, (255, 255, 0))
+            draw(original_img, vertexes, (255, 255, 0), scale=Config.stride*ratio)
             centroid = tuple([int(point * Config.stride * ratio) for point in object[0]])
             original_img = cv2.circle(original_img, centroid, 5, (255, 255, 0), -1)
             obj_2d_points = np.array(obj_2d_points, dtype=float)
@@ -121,11 +132,11 @@ for path in path_to_sequences:
                         rotate_quaternion = Quaternion.from_axis_rotation(location, rotate_angle)
                         quaternion = rotate_quaternion.cross(quaternion)
                     vertexes = [tuple(p) for p in projected_points]
-            draw(original_img, vertexes)
+                draw(original_img, vertexes)
         # print('save output/stage1/img_{}.png' .format(id))
         # cv2.imwrite('output/stage1/img_{}.png' .format(id), original_img)
     cv2.imshow('img', original_img)
-    cv2.waitKey(5000)
+    cv2.waitKey(1000)
 
 
 
